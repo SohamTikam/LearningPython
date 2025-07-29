df344ba67796a68c92d178582c661edc45b0c0a5
Soham Tikam <sohamtikam10@gmail.com>
2025-07-29 20:11:37 +0530

    list

---
+++ list.py
@@ -0,0 +1,14 @@
+x = [1,2,3, 'hello', True , 'hi' ]
+print(x)
+
+x = [1,2,3, 'hello', True , 'hi' ]
+print(x.pop())
+
+x = [1,2,3, 'hello', True , 'hi' ]
+x.append('india')
+print(x)
+
+x = [1,2,3, 'hello', True , 'hi' ]
+x.extend([1,2,3,4,5,6,7,8,9])
+print(x)
+print(len(x))
