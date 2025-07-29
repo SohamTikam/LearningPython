d86811c02d5fa30208ff5da877fc943950a270dd
Soham Tikam <sohamtikam10@gmail.com>
2025-07-29 08:26:53 +0530

    conditionaloperation

---
+++ condiotionaloperators.py
@@ -0,0 +1,9 @@
+# == // egual to , != //not equal to , <= lower than equal to , >= greater than equal to , > greater , < lower
+num_1 = 3
+num_2 = 4
+print(num_1 == num_2)
+print((num_1 != num_2))
+
+
+result = 6==6
+print (result)
