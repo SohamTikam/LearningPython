972db09a58a7ee98386264cab05190be8e8dd2cc
Soham Tikam <sohamtikam10@gmail.com>
2025-07-29 08:01:56 +0530

    arthematic operation

---
+++ arthematic operation.py
@@ -0,0 +1,29 @@
+x = 5
+y = 2
+result = x + y
+print (result)
+ # itis giving float value
+x = 3
+y = 2
+result = x / y
+print (result)
+# to avoid float value
+x = 3
+y = 2
+result = int( x / y )
+print (result)
+x = 3
+y = 2
+result = x ** y
+print (result)
+x = 3
+y = 2
+result = (x / y) + 10
+print (result)
+
+num = input ("Enter number: ")
+print(int(num) + 10)
+
+num = input ("Enter number: ")
+print(float(num) + 10)
+
