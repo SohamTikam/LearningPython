c29f952d1422c4a0b9409a6abe64bad5da3889dc
Soham Tikam <sohamtikam10@gmail.com>
2025-07-29 08:46:25 +0530

    conditionaloperation

---
+++ chainedcondition.py
@@ -0,0 +1,26 @@
+# or , not , and
+x = 3
+y = 2
+z = 5
+result_1 = x == y
+result_2 = x != y
+result_3 = z> x + 2 
+result_4 = result_1 or result_2
+print(result_4)
+
+x = 3
+y = 2
+z = 5
+result_1 = x == y
+result_2 = x != y
+result_3 = z> x + 2 
+print(not(True))
+
+
+x = 3
+y = 2
+z = 5
+result_1 = x == y
+result_2 = x != y
+result_3 = z> x + 2 
+print(not(False and True or False ))
