5d933cf7b0c69a7fe74ab045011b58f8a074993b
Soham Tikam <sohamtikam10@gmail.com>
2025-08-01 19:53:53 +0530

    'whileloop'

---
+++ whileloop.py
@@ -0,0 +1,15 @@
+x = [1,2,34,56,78,21,34]
+
+i = 2
+while i < 10 :
+    print('run')
+    i += 1
+
+x = [1,2,34,56,78,21,34]
+
+i = 2
+while True:
+    print('run')
+    i += 1
+    if i==10 :
+     break        
