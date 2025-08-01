8e5d685c6db3f5d65c4e62701be649030676598a
Soham Tikam <sohamtikam10@gmail.com>
2025-08-01 19:40:48 +0530

    'forloop'

---
+++ forloop.py
@@ -0,0 +1,18 @@
+# START , STOP , STEP 
+for i in range (10) :
+     print(i)
+
+for i in range (1,10) :
+     print(i)     
+
+for i in range (1,10,2) :
+     print(i)  
+
+x = [1,2,3,4,5,67,76]    
+for i in range (len(x)) :
+    print(x[i])
+
+
+x = [1,2,3,4,5,67,76]    
+for i, element in enumerate (x) :
+    print(i , element)    
