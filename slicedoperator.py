e0c98b7cf7c85931b718cfca027b732de32a8326
Soham Tikam <sohamtikam10@gmail.com>
2025-08-01 20:02:45 +0530

    'slicedoperators'

---
+++ slicedoperator.py
@@ -0,0 +1,20 @@
+x = [1,2,3,4,5,6,7,8]
+y = ['hi','hello' , 'goodbye']
+z = 'hello'
+
+sliced = x[1:7]
+print(sliced)
+
+sliced = x[1:7:2]
+print(sliced)
+
+sliced = x[::-1]
+print(sliced)
+
+sliced = y[1:]
+print(sliced)
+
+sliced = y[::-1]
+print(sliced)
+
+
